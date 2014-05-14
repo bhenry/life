c275cab691a34d4af05a21ca01c8509051620c06
Brandon Henry <brandon.m.henry@gmail.com>
2014-05-14 00:16:05 -0400

    checkin compiled files

---
+++ out/life/core.js
@@ -0,0 +1,10 @@
+// Compiled by ClojureScript 0.0-2197
+goog.provide('life.core');
+goog.require('cljs.core');
+/**
+* I don't do a whole lot.
+*/
+life.core.foo = (function foo(x){return console.log(x,"Hello, World!");
+});
+
+//# sourceMappingURL=core.js.map
